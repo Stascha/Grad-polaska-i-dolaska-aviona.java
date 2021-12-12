33a3d5dff8e4ff46bde41b938c85580e6a8ea02f
Stascha <stasarasic@gmail.com>
2021-12-12 22:33:46 +0100

    Create Grad-polaska-i-dolaska-aviona.java

---
+++ Grad-polaska-i-dolaska-aviona.java
@@ -0,0 +1,75 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class FindDepartureAndDestination {
+
+    public static String[] find(List<List<String>> passes) 
+    {
+       
+       
+        Set<String> src  = new HashSet<String> (); 
+        Set<String> dest = new HashSet<String> (); 
+        
+        for(int i=0;i<passes.size();i++)
+        {
+            List<String> path = passes.get(i);
+            src.add(path.get(0));
+            dest.add(path.get(1));
+           
+        }
+         
+        Set<String> intersection = new HashSet<String>(src);
+        intersection.retainAll(dest);
+        
+        
+        src.removeAll(intersection);
+        dest.removeAll(intersection);
+        
+        String[] s = src.toArray(new String[0]);
+        String[] d = dest.toArray(new String[0]);
+        
+        String[] r = new String[2];
+        r[0] = s[0];
+        r[1] = d[0];
+        
+        return r;
+        
+    }
+
+    public static void main(String[] args) {
+        
+        
+        List<List<String>> boardingPass = new ArrayList<>();
+        
+        List<String> bp1 = new ArrayList<>();
+        bp1.add("Dallas"); bp1.add("New York");
+        boardingPass.add(bp1);
+       
+        List<String> bp2 = new ArrayList<>();
+        bp2.add("Houston"); bp2.add("Berlin");
+        boardingPass.add(bp2);
+      
+        List<String> bp3 = new ArrayList<>();
+        bp3.add("Austin");bp3.add("Dallas");
+        boardingPass.add(bp3);
+        
+        List<String> bp4 = new ArrayList<>();
+        bp4.add("New York");bp4.add("Seattle");
+        boardingPass.add(bp4);
+        
+        List<String> bp5 = new ArrayList<>();
+        bp5.add("Seattle");bp5.add("Houston");
+        boardingPass.add(bp5);
+        
+        List<String> bp6 = new ArrayList<>();
+        bp6.add("Tokyo");bp6.add("Austin");
+        boardingPass.add(bp6);
+        
+        String[] r;
+        
+        r = find(boardingPass);
+        System.out.println("Departure: " + r[0] + ", Destination: " + r[1]);
+    }
+}
